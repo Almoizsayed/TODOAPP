dcedd734d7e2932b816028e35fc4b8774ffba248
Almoiz Sayyed <almoizs@heaptrace.net>
2024-06-04 16:55:25 +0530

    Styling TODO Items

--- TODOAPP/todoapp/src/components/Form.jsx
+++ TODOAPP/todoapp/src/components/Form.jsx
@@ -1,3 +1,4 @@
+import styles from "./form.module.css";
 import { useState } from "react";
 export default function Form({ todos, setTodos }) {
   const [todo, setTodo] = useState("");
@@ -7,13 +8,19 @@ export default function Form({ todos, setTodos }) {
     setTodo("");
   }
   return (
-    <form onSubmit={handlesubmit}>
-      <input
-        type="text"
-        onChange={(e) => setTodo(e.target.value)}
-        value={todo}
-      />
-      <button type="submit">Add</button>
+    <form className={styles.todoform} onSubmit={handlesubmit}>
+      <div className={styles.inputcontainer}>
+        <input
+          className={styles.modernInput}
+          type="text"
+          placeholder="Enter todo item"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button className={styles.modernButton} type="submit">
+          Add
+        </button>
+      </div>
     </form>
   );
 }
