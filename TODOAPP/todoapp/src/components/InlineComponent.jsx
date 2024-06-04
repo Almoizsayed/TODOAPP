32fa2963a9a7f44e9537cd13d414fd28d60fc291
Almoiz Sayyed <almoizs@heaptrace.net>
2024-06-04 14:48:39 +0530

    CSS Modules for Styling

--- TODOAPP/todoapp/src/components/InlineComponent.jsx
+++ TODOAPP/todoapp/src/components/InlineComponent.jsx
@@ -1,8 +1,8 @@
-import "../CSS/Style.css";
+import styles from "./inlinecomponent.module.css";
 export default function () {
   return (
     <div>
-      <h1 className="header">Almoiz you are doing Fantastic</h1>
+      <h1 className={styles.header}>Almoiz you are doing Fantastic</h1>
     </div>
   );
 }
